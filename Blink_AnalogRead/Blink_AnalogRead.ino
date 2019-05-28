bcc0e993b5d91dd9b49a8216523e5f7c1b688166
Bhuvaneshwar Kanade <bhuvankanade12@gmail.com>
2019-05-28 12:13:55 +0530

    external led on gpio 40 aurdino mega

--- Blink_AnalogRead/Blink_AnalogRead.ino
+++ Blink_AnalogRead/Blink_AnalogRead.ino
@@ -70,15 +70,15 @@ void TaskBlink(void *pvParameters)  // This is a task.
   modified 2 Sep 2016
   by Arturo Guadalupi
 */
-
+  #define LED  40
   // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
